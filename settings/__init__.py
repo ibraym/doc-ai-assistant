ced958af0627d4c57a2b10b8c9821457248d7868
Ibrahem Mouhamad <ibrahem.y.mouhamad@gmail.com>
2023-05-22 00:59:04 +0700

    Added settings module

---
+++ settings/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2023 Ibrahem Mouhamad
+#
+# SPDX-License-Identifier: MIT
+
+import os
+
+env = os.environ.get('APP_ENV', 'develop')
+
+if env == 'development':
+    from development import *
+elif env == 'production':
+    from production import *
+else:
+    from testing import *
---
+++ settings/base.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2023 Ibrahem Mouhamad
+#
+# SPDX-License-Identifier: MIT
+
+ANALYZER = {
+    'huggingface': {
+        'SentenceTransformers': 'sentence-transformers/all-MiniLM-L6-v2'
+    }
+}
